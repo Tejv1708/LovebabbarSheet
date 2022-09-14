3be4c252167c7e9db7f30c3743a9c67838da88f7
Tejv1708 <80191104+Tejv1708@users.noreply.github.com>
2022-09-14 10:31:31 +0530

    Create 2Sum.cpp

---
+++ 2Sum.cpp
@@ -0,0 +1,13 @@
+
+// In this we use the HashMap because it take minimum time for run . But when we use two pointer methode then it exceeds the time limit .
+
+vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> indices;
+        for (int i = 0; i < nums.size(); i++) {
+            if (indices.find(target - nums[i]) != indices.end()) {
+                return {indices[target - nums[i]], i};
+            }
+            indices[nums[i]] = i;
+        }
+        return {};
+    }
